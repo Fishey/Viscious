766f042e0d898b03549477a8cb8d7113dd1df46b
Fishey <randybrussen@gmail.com>
2015-03-18 13:43:28 +0100

    Oops

---
+++ Components/IntVec2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace GXPEngine
+{
+	public class IntVec2 
+	{
+		public int x = 0;
+		public int y = 0;
+		public static IntVec2 zero { get { return new IntVec2 (0, 0); } }
+
+
+		public IntVec2 (int pX = 0, int pY = 0)
+		{
+			x = pX;
+			y = pY;
+		}
+
+		public override string ToString ()
+		{
+			return String.Format ("({0}, {1})", x, y);
+		}
+
+		public static bool operator == (IntVec2 a, IntVec2 b)
+		{
+			if (System.Object.ReferenceEquals (a, b))
+				return true;
+			if (((object)a == null || ((object)b == null)))
+			{
+				return false;
+			}
+
+			return a.x == b.x && a.y == b.y;
+		}
+
+		public static bool operator != (IntVec2 a, IntVec2 b)
+		{
+			return !(a == b);
+		}
+
+		public Vec2 ToVec2()
+		{
+			return new Vec2 (this.x, this.y);
+		}
+
+		public IntVec2 Add (IntVec2 other) {
+			x += other.x;
+			y += other.y;
+			return this;
+		}
+
+		public IntVec2 Sub (IntVec2 other) {
+			x -= other.x;
+			y -= other.y;
+			return this;
+		}
+
+		public IntVec2 Clone() {
+			return new IntVec2 (x, y);
+		}
+
+		public bool Equals (IntVec2 other)
+		{
+			return other != null && x == other.x && y == other.y;
+		}
+
+	}
+}
+
---
+++ Components/Vec2.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace GXPEngine
+{
+	public class Vec2 
+	{
+		public static Vec2 zero { get { return new Vec2(0,0); }}
+		public static Vec2 temp = new Vec2 ();
+
+		public float x = 0;
+		public float y = 0;
+
+		public Vec2 (float pX = 0, float pY = 0)
+		{
+			x = pX;
+			y = pY;
+		}
+
+		public static bool operator == (Vec2 a, Vec2 b)
+		{
+			if (System.Object.ReferenceEquals (a, b))
+				return true;
+			if (((object)a == null || ((object)b == null)))
+				{
+					return false;
+				}
+
+				return a.x == b.x && a.y == b.y;
+		}
+
+		public IntVec2 ToIntVec2()
+		{
+			return new IntVec2 ((int)x, (int)y);
+		}
+
+		public static bool operator != (Vec2 a, Vec2 b)
+		{
+			return !(a == b);
+		}
+
+		public override string ToString ()
+		{
+			return String.Format ("({0}, {1})", x, y);
+		}
+
+		public Vec2 Add (Vec2 other) {
+			x += other.x;
+			y += other.y;
+			return this;
+		}
+
+		public Vec2 Substract (Vec2 other) {
+			x -= other.x;
+			y -= other.y;
+			return this;
+		}
+
+		public float Length() {
+			return (float)Math.Sqrt (x * x + y * y);
+		}
+
+		public Vec2 Normalize () {
+			if (x == 0 && y == 0) {
+				return this;
+			} else {
+				return Scale (1/Length ());
+			}
+		}
+
+		public Vec2 Clone() {
+			return new Vec2 (x, y);
+		}
+
+		public Vec2 Scale (float scalar) {
+			x *= scalar;
+			y *= scalar;
+			return this;
+		}
+
+		public Vec2 Set (float pX, float pY) {
+			x = pX;
+			y = pY;
+			return this;
+		}
+
+		public Vec2 Normal() {
+			return new Vec2 (-this.y, this.x).Normalize();
+		}
+
+		public Vec2 Truncate (float length) {
+			if (Length () > length) {
+				Normalize ().Scale (length);
+			}
+			return this;
+		}
+
+		public float Dot (Vec2 other) {
+			return (float)(this.x * other.x) + (this.y * other.y);
+		}
+
+		public Vec2 Reflect(Vec2 normal, float bounciness = 1)
+		{
+			/*
+			float Px = this.x - (1 + bounciness) * (this.Dot (normal) * normal.x);
+			float Py = this.y - (1 + bounciness) * (this.Dot (normal) * normal.y);
+
+			this.x = Px;
+			this.y = Py;
+			*/
+
+			return this.Substract(normal.Scale((1+bounciness) * this.Dot(normal)));
+		}
+
+		public void SetAngleRadians(float radians)
+		{
+			float length = Length();
+			this.x = (float) (length * Math.Cos (radians));
+			this.y = (float) (length * Math.Sin (radians));
+		}
+
+		public void SetAngleDegrees(float degrees)
+		{
+			SetAngleRadians (degrees * (float)Math.PI / 180);
+		}
+
+		public float GetAngleRadians()
+		{
+			return (float)Math.Atan2 (this.y, this.x);
+		}
+
+		public float GetAngleDegrees()
+		{
+			return (float)GetAngleRadians () * (float)(180 / Math.PI);
+		}
+
+		public void RotateDegrees(float degrees)
+		{
+			SetAngleDegrees (GetAngleDegrees () + degrees);
+		}
+
+		public void RotateRadians(float radians)
+		{
+			SetAngleRadians (GetAngleRadians () + radians);
+		}
+	}
+}
+
